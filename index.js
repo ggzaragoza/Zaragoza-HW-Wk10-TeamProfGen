596337778a7264debce6502a8bfb255b676968ad
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-10-05 21:41:48 -0500

    Added 'inquirer' prompts to collect team info, pushed info to teamMembers array

--- index.js
+++ index.js
@@ -1,59 +1,288 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const jest = require('jest');
 
-const employee = require('./lib/employee.js');
+// const employee = require('./lib/employee.js');
+const Manager = require('./lib/manager.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: ' This application will generate a new team profile page for your team. A team manager is required for each team, please enter your first and last name. (required)',
-            name: 'managerName',
-            validate: (input) => {
-                if (input === '') {
-                    return console.log('Please enter your full name.')
-                } else {
-                    return true;
+
+const teamMembers = [];
+
+
+addManager = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: ' This application will generate a new team profile page for your team. A team manager is required for each team, please enter your first and last name. (required)',
+                name: 'managerName',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log('Please enter your full name.')
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is your manager's employee ID? (required)",
+                name: 'managerId',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter your manager's employee ID.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is your manager's email address? (required)",
+                name: 'managerEmail',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter your manager's email address.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is your manager's office number? (required)",
+                name: 'managerOffice',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter your manager's office number.")
+                    } else {
+                        return true;
+                    }
                 }
             }
-        },
-        {
-            type: 'input',
-            message: "What is your manager's employee ID? (required)",
-            name: 'managerId',
-            validate: (input) => {
-                if (input === '') {
-                    return console.log("Please enter your manager's employee ID.")
-                } else {
-                    return true;
+        ])
+        .then((data) => {
+            const teamManager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOffice);
+            teamMembers.push(teamManager);
+
+            buildTeam();
+            // console.log(teamManager);
+            // return teamManager;
+            // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+
+            // fs.writeFile('index.html', teamContent(data), (err) => 
+            //     err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
+        });
+};
+
+
+addEngineer = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Please enter the Engineer's full name. (required)",
+                name: 'engineerName',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log('Please enter a name for the Engineer.')
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the Engineer's employee ID? (required)",
+                name: 'engineerId',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter the Engineer's employee ID.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the Engineer's email address? (required)",
+                name: 'engineerEmail',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter the Engineer's email address.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the Engineer's Github username? (required)",
+                name: 'engineerGitHub',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter the Engineer's GitHub username.")
+                    } else {
+                        return true;
+                    }
                 }
             }
-        },
-        {
-            type: 'input',
-            message: "What is your manager's email address? (required)",
-            name: 'managerEmail',
-            validate: (input) => {
-                if (input === '') {
-                    return console.log("Please enter your manager's email address.")
-                } else {
-                    return true;
+        ])
+        .then((data) => {
+            const newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGitHub);
+            teamMembers.push(newEngineer);
+
+            buildTeam();
+        });
+};
+
+
+addIntern = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Please enter the Intern's full name. (required)",
+                name: 'internName',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log('Please enter a name for the Intern.')
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the Intern's employee ID? (required)",
+                name: 'internId',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter the Intern's employee ID.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is the Intern's email address? (required)",
+                name: 'internEmail',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("Please enter the Intern's email address.")
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "At which institution is the Intern currently enrolled? (required)",
+                name: 'internSchool',
+                validate: (input) => {
+                    if (input === '') {
+                        return console.log("This field is required. If not attending school, enter 'n/a.'")
+                    } else {
+                        return true;
+                    }
                 }
             }
-        },
-        {
-            type: 'input',
-            message: "What is your manager's office number? (required)",
-            name: 'managerOffice',
-            validate: (input) => {
-                if (input === '') {
-                    return console.log("Please enter your manager's office number.")
-                } else {
-                    return true;
-                }
+        ])
+        .then((data) => {
+            const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+            teamMembers.push(newIntern);
+
+            buildTeam();
+            // console.log(teamManager);
+            // return newEngineer;
+            // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+
+            // fs.writeFile('index.html', teamContent(data), (err) => 
+            //     err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
+        });
+};
+
+
+function buildTeam() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Would you like to add a new team member?',
+                name: 'profileOptions',
+                choices: ['Add Engineer', 'Add Intern', 'Generate Team Profile']
             }
-        }
+        ])
+        .then((data) => {
+            if (data.profileOptions === 'Add Engineer') {
+                addEngineer();
+            } else if (data.profileOptions === 'Add Intern') {
+                addIntern();
+            } else {
+                generateTeamProfile();
+            }
+        });
+}
+
+function generateTeamProfile() {
+    console.log(teamMembers);
+}
+
+
+addManager();
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             message: ' This application will generate a new team profile page for your team. A team manager is required for each team, please enter your first and last name. (required)',
+//             name: 'managerName',
+//             validate: (input) => {
+//                 if (input === '') {
+//                     return console.log('Please enter your full name.')
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: 'input',
+//             message: "What is your manager's employee ID? (required)",
+//             name: 'managerId',
+//             validate: (input) => {
+//                 if (input === '') {
+//                     return console.log("Please enter your manager's employee ID.")
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: 'input',
+//             message: "What is your manager's email address? (required)",
+//             name: 'managerEmail',
+//             validate: (input) => {
+//                 if (input === '') {
+//                     return console.log("Please enter your manager's email address.")
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         },
+//         {
+//             type: 'input',
+//             message: "What is your manager's office number? (required)",
+//             name: 'managerOffice',
+//             validate: (input) => {
+//                 if (input === '') {
+//                     return console.log("Please enter your manager's office number.")
+//                 } else {
+//                     return true;
+//                 }
+//             }
+//         }
         // {
         //     type: 'input',
         //     message: 'Describe how a user should utilize or interact with your project. (optional)',
@@ -99,87 +328,16 @@ inquirer
         //         }
         //     }
         // }
-    ])
-    .then((data) => {
+    // ])
+    // .then((data) => {
         // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
 
-        fs.writeFile('index.html', teamContent(data), (err) => 
-            err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
-    });
+    //     fs.writeFile('index.html', teamContent(data), (err) => 
+    //         err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
+    // });
 
 
-teamContent = (data) => {
-// let tableofCont = `
-// ## Table of Contents`;
-
-// if (data.installation !== ``) { tableofCont += `
-// - [Installation](#installation)`;
-// }
-// if (data.usage !== ``) { tableofCont += `
-// - [Usage](#usage)`;
-// }
-// if (data.testing !== ``) { tableofCont += `
-// - [Testing](#testing)`;
-// }
-// if (data.contributing !== ``) { tableofCont += `
-// - [Contributions](#contributions)`;
-// }
-// tableofCont += `
-// - [License](#license)
-// - [Contact](#contact)`;
-
-
-// let badge = ``;
-
-// if (data.license === 'MIT') { badge += `
-// ![MIT License](/badges/license-MIT-red.svg)`
-// };
-// if (data.license === 'ISC') { badge += `
-// ![ISC License](/badges/license-ISC-blue.svg)`
-// };
-// if (data.license === 'Apache 2.0') { badge += `
-// ![Apache 2.0 License](/badges/license-Apache-orange.svg)`
-// };
-// if (data.license === 'BSD 3-Clause') { badge += `
-// ![BSD 3-Clause License](/badges/license-BSD-green.svg)`
-// };
-
-
-// let content = `
-// # ${data.title}`;
-
-// content += `
-// ## Description
-// ${data.description}
-// ${badge}`;
-
-// content += tableofCont;
-
-// if (data.installation !== ``) { content += `
-// ## Installation
-// ${data.installation}`;
-// }
-// if (data.usage !== ``) { content += `
-// ## Usage
-// ${data.usage}`;
-// }
-// if (data.testing !== ``) { content += `
-// ## Testing
-// ${data.testing}`;
-// }
-// if (data.contributing !== ``) { content += `
-// ## Contributions
-// ${data.contributing}`;
-// }
-
-// content += `
-// ## License
-// \u00A9 ${copyrightYear} ${data.fullname}\n
-// This project is protected under the ${data.license} license. For more information, please visit [https://opensource.org/licenses](https://opensource.org/licenses).
-
-// ## Contact
-// This project was created by ${data.fullname} and can be found at [https://github.com/${data.author}](https://github.com/${data.author}).\n
-// For further information, feel free to email the author at ${data.email} with any questions about the project or regarding future updates or other issues.`;
+// teamContent = (data) => {
 
 // return content;
-};
+// };
