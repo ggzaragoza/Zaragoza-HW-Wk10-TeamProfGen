fba69aa916c10968c815d05e3ff99f1bc94c18f2
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-10-06 01:21:43 -0500

    index.js will generate most basic HTML file with data from user input

--- index.js
+++ index.js
@@ -67,8 +67,7 @@ addManager = () => {
             teamMembers.push(teamManager);
 
             buildTeam();
-            // console.log(teamManager);
-            // return teamManager;
+
             // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
 
             // fs.writeFile('index.html', teamContent(data), (err) => 
@@ -195,17 +194,11 @@ addIntern = () => {
             teamMembers.push(newIntern);
 
             buildTeam();
-            // console.log(teamManager);
-            // return newEngineer;
-            // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
-
-            // fs.writeFile('index.html', teamContent(data), (err) => 
-            //     err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
         });
 };
 
 
-function buildTeam() {
+buildTeam = () => {
     inquirer
         .prompt([
             {
@@ -226,115 +219,15 @@ function buildTeam() {
         });
 }
 
-function generateTeamProfile() {
+generateTeamProfile = () => {
     console.log(teamMembers);
+
+    fs.writeFile('./dist/index.html', JSON.stringify(teamMembers), (err) => 
+    err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
 }
 
 
 addManager();
-
-// inquirer
-//     .prompt([
-//         {
-//             type: 'input',
-//             message: ' This application will generate a new team profile page for your team. A team manager is required for each team, please enter your first and last name. (required)',
-//             name: 'managerName',
-//             validate: (input) => {
-//                 if (input === '') {
-//                     return console.log('Please enter your full name.')
-//                 } else {
-//                     return true;
-//                 }
-//             }
-//         },
-//         {
-//             type: 'input',
-//             message: "What is your manager's employee ID? (required)",
-//             name: 'managerId',
-//             validate: (input) => {
-//                 if (input === '') {
-//                     return console.log("Please enter your manager's employee ID.")
-//                 } else {
-//                     return true;
-//                 }
-//             }
-//         },
-//         {
-//             type: 'input',
-//             message: "What is your manager's email address? (required)",
-//             name: 'managerEmail',
-//             validate: (input) => {
-//                 if (input === '') {
-//                     return console.log("Please enter your manager's email address.")
-//                 } else {
-//                     return true;
-//                 }
-//             }
-//         },
-//         {
-//             type: 'input',
-//             message: "What is your manager's office number? (required)",
-//             name: 'managerOffice',
-//             validate: (input) => {
-//                 if (input === '') {
-//                     return console.log("Please enter your manager's office number.")
-//                 } else {
-//                     return true;
-//                 }
-//             }
-//         }
-        // {
-        //     type: 'input',
-        //     message: 'Describe how a user should utilize or interact with your project. (optional)',
-        //     name: 'usage',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'How was/can your project be tested? (optional)',
-        //     name: 'testing',
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'How can users or other developers contribute to this project? (optional)',
-        //     name: 'contributing',
-        // },
-        // {
-        //     type: 'list',
-        //     message: 'Under which license will your project be covered?',
-        //     name: 'license',
-        //     choices: ['MIT', 'ISC', 'Apache 2.0', 'BSD 3-Clause'],
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Please enter your GitHub username. (required)',
-        //     name: 'author',
-        //     validate: (input) => {
-        //         if (input === '') {
-        //             return console.log("Please provide your GitHub username as author of this README.")
-        //         } else {
-        //             return true;
-        //         }
-        //     }
-        // },
-        // {
-        //     type: 'input',
-        //     message: 'Please provide an email address where you can be reached for additional questions about your new project. (required)',
-        //     name: 'email',
-        //     validate: (input) => {
-        //         if (input === '') {
-        //             return console.log("An email address for contact purposes is required to finish this README.")
-        //         } else {
-        //             return true;
-        //         }
-        //     }
-        // }
-    // ])
-    // .then((data) => {
-        // const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
-
-    //     fs.writeFile('index.html', teamContent(data), (err) => 
-    //         err ? console.error(err) : console.log('Success! A README for your new project has been created.'))
-    // });
 
 
 // teamContent = (data) => {
